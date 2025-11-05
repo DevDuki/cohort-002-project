93cae93780d08b55f2764d6362be4f907a64c3ff
Matt Pocock <mattpocockvoice@gmail.com>
2025-11-05 16:21:38 +0000

    06.01.04 Added memory extraction

--- src/app/chat.tsx
+++ src/app/chat.tsx
@@ -69,6 +69,9 @@ export const Chat = (props: { chat: DB.Chat | null }) => {
         router.refresh();
       }
     },
+    onFinish: () => {
+      router.refresh();
+    },
     generateId: () => crypto.randomUUID(),
   });
 
